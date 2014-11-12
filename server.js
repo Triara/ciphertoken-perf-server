f549d295e59f462c7950b5eb99f8c6e2cb8dd8c5
Kristina Lyubomirova <kristina.lyubomirova@bbva.com>
2014-11-12 23:58:27 +0100

    validation for existing body params in /start

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-var restify = require('restify');
-var tester = require('./tester');
+var restify = require('restify'),
+    tester = require('./tester'),
+    async = require('async');
 
 var server = restify.createServer({
     name: 'CipherTokenPerformanceTestingServer',
     version: '0.0.1'
 });
 
+const DEFAULT_TIMES = 300;
+const DEFAULT_ITERATIONS = 3;
+
 server.use(restify.bodyParser({ mapParams: false }));
 
-
 server.post("/start", function(req, res, next) {
-    var startingTimes = req.body.startingTimes || 300;
-    var iterations = req.body.iterations || 3;
 
-    tester.runTests(startingTimes, iterations);
-    var body = tester.getResultsFromAllPerfTests();
+    async.parallel([
+        function(){
+            var startingTimes = setStartingTimes(req);
+            var iterations = setIterations(req);
 
-    res.send(200, body);
-    return next();
+            tester.runTests(startingTimes, iterations);
+        },
+        function(){
+            res.send(200, 'Performance tests in progress');
+            return next();
+        }
+    ]);
 });
 
 server.get("/outCome", function (req, res, next) {
@@ -30,3 +38,24 @@ server.get("/outCome", function (req, res, next) {
 server.listen(process.env.PORT || 3000, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
+
+
+function setStartingTimes(req) {
+    var startingTimes = '';
+    if (typeof req.body !== 'undefined' && typeof req.body.startingTimes !== 'undefined' && req.body.startingTimes !== null) {
+        startingTimes = req.body.startingTimes;
+    } else {
+        startingTimes = DEFAULT_TIMES;
+    }
+    return startingTimes;
+}
+
+function setIterations(req) {
+    var iterations = '';
+    if (typeof req.body !== 'undefined' && typeof req.body.iterations !== 'undefined' && req.body.iterations !== null) {
+        iterations = req.body.iterations;
+    } else {
+        iterations = DEFAULT_ITERATIONS;
+    }
+    return iterations;
+}
